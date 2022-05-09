be24edae3f20b45237a0698b4eb4a6ae67d89704
gardarvt <gardar@users.noreply.github.com>
2022-05-09 15:47:36 +0000

    Remove requests install debug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ set -e pipefail
 
 python3 -m venv .venv
 source .venv/bin/activate
-python3 -m pip install 'gitlint>=0.15.0,<0.16.0 requests'
+python3 -m pip install 'gitlint>=0.15.0,<0.16.0'
 
 gitlint install-hook
 
